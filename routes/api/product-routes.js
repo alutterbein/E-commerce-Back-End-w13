0cdecb6914ff72281592452e6cfc271eab91b858
alutterbein <alutterbein@gmail.com>
2024-06-23 15:34:35 -0700

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({ include: [{model: Category }] });
+    res.status(200).json(productData);}
+  catch (err) {
+    res.status(500).json(err);}
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, { include: [{ model: Category }, { model: Tag }] });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }});
 
 // create new product
 router.post('/', (req, res) => {
@@ -48,8 +59,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
+router.put('/:id', async (req, res) => {
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -91,9 +101,21 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
+//delete product
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  } catch (err) { res.status(500).json(err); }
 });
 
 module.exports = router;
